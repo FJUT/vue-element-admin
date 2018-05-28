5f49ccac45229cb39f9d8a2fa35c249ee9255305
KainyGuo <gt@kainy.cn>
2018-05-28 21:29:04 +0800

    mock OK

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"https://5v6owrzy.qcloud.la/web"'
+  BASE_API: '"/web"'
 }
--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/web': {
+        target: 'http://5v6owrzy.qcloud.la', // 接口的域名
+        // secure: false,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true // 如果接口跨域，需要进行这个参数配置
+        // pathRewrite: {
+        //   '^/api': ''
+        // }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -45,7 +54,7 @@ module.exports = {
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false,
+    cssSourceMap: false
   },
 
   build: {
